9988d007e08da3cad84bf486d6a24c6c03bc1ece
Paweł Sokołowski <p.k.sokolowski@outlook.com>
2022-01-22 19:26:41 +0100

    use cookies to store and run selected sample

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import sampleSample from './samples/sampleSample.js';
 
+const COOKIE_NAME_SELECTED_SAMPLE_ID = 'selectedSampleId';
+
 const samplesList = document.getElementById("samplesList")
 
 const samples = [
@@ -12,5 +14,26 @@ const samples = [
 samples.forEach((sample) => {
     const html = `<p id="${sample.id}">${sample.id}</p>`;
     samplesList.innerHTML += html;
-    document.getElementById(`${sample.id}`).addEventListener("click", sample.fun);
+    document.getElementById(`${sample.id}`).addEventListener("click", function () {
+        document.cookie = `${COOKIE_NAME_SELECTED_SAMPLE_ID}=${sample.id}`;
+        runSelectedSample();
+    });
 });
+
+function runSelectedSample() {
+    const selectedSampleId = getCookieValueByName(COOKIE_NAME_SELECTED_SAMPLE_ID);
+    if (!selectedSampleId) { return; }
+    const sample = samples.find(sample => sample.id === selectedSampleId);
+    if (sample) {
+        sample.fun();
+    }
+}
+
+function getCookieValueByName(cookieName) {
+    const cookie = document.cookie.split('; ').find(row => row.startsWith(cookieName));
+    if (!cookie) { return null; }
+    const value = cookie.split('=')[1];
+    return value;
+}
+
+runSelectedSample();
